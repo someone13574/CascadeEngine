b002b69bccf0f4b1495dfad8d00f0f8bb725dfdf
Owen Law <owenlaw222@gmail.com>
2022-08-07 09:56:50 -0400

    Fixed smooth edges

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,13 @@ void Update(Cascade_Core::Application* instance_ptr)
 
     std::shared_ptr<Cascade_Graphics::Camera> camera_ptr = main_window_ptr->Get_Renderer()->Get_Camera();
 
-    // camera_ptr->Set_Position({std::sin(elapsed_seconds) * 2.2, std::sin(elapsed_seconds * 3.0) * 2, std::cos(elapsed_seconds) * 2.1});
-    // camera_ptr->Look_At(Cascade_Graphics::Vector_3<double>(std::sin(elapsed_seconds + 0.1) * 2.2, std::sin((elapsed_seconds + 0.1) * 3.0) * 2, std::cos(elapsed_seconds + 0.1) * 2.1) * 0.75);
-
-    camera_ptr->Set_Position({std::sin(elapsed_seconds) * 2.2, 0, std::cos(elapsed_seconds) * 2.2});
-    camera_ptr->Look_At({0, 0, 0});
+    camera_ptr->Set_Position({std::sin(elapsed_seconds) * 2.2, std::sin(elapsed_seconds * 3.0) * 2, std::cos(elapsed_seconds) * 2.1});
+    camera_ptr->Look_At(Cascade_Graphics::Vector_3<double>(std::sin(elapsed_seconds + 0.1) * 2.2, std::sin((elapsed_seconds + 0.1) * 3.0) * 2, std::cos(elapsed_seconds + 0.1) * 2.1) * 0.75);
 }
 
 double Volume_Sample_Function(Cascade_Graphics::Vector_3<double> position)
 {
-    return position.Length() - 1.8 + (std::sin(position.m_x * 10.0) * std::sin(position.m_y * 10.0) * std::sin(position.m_z * 10.0) * 0.1);
+    return position.Length() - 1.8 + (std::sin(position.m_x * 10.0) * std::sin(position.m_y * 10.0) * std::sin(position.m_z * 10.0) * 0.2);
 }
 
 int main()
